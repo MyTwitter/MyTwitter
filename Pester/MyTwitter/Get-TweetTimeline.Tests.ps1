f097e564248a2e1d16b54ddf5b55c13dceeec9ac
Stefan Stranger <stefan.stranger@outlook.com>
2014-10-27 15:33:39 +0100

    Added Pester test script for Get-TweetTimeline
    Added Pester test script for Get-TweetTimeline function

---
+++ Pester/MyTwitter/Get-TweetTimeline.Tests.ps1
@@ -0,0 +1,40 @@
+﻿<#
+  Pester provides a framework for running Unit Tests to execute and validate PowerShell commands inside of PowerShell.
+  More info can be found here: 
+  https://github.com/pester/Pester
+
+  To test run the following command:
+  invoke-pester from ..\Pester\MyTwitter folder
+
+#>
+
+Remove-Module MyTwitter -Force -ErrorAction SilentlyContinue
+
+$scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+cd $scriptRoot
+cd ..\..
+Import-Module .\MyTwitter.psm1 -Force -ErrorAction Stop
+
+#Variables:
+$username = "sstranger"
+
+Describe "Get-TweetTimeline" {
+Context "Mandatory UserName parameter is provided" {
+    It "outputs 'User screen_name properties'" {
+                ((get-tweettimeline -Username $username -MaximumTweets 1).user).screen_name | Should Be "$username"
+    }
+  }
+Context "MaxTweets parameter of more than 200 is entered." {
+      It "fails" {
+           { Get-TweetTimeline -MaximumTweets 201} | Should Throw
+      }
+  }
+
+Context "no UserName parameter is provided. Hit enter during Pester test." {
+      It "fails" {
+           { Get-TweetTimeline } | Should Throw
+      }
+  }
+}
+
+cd $scriptRoot
