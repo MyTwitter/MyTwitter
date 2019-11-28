7baf51036144b10b8a0566c86ed8dc97ce4e103a
Adam Bertram <adam@Adams-MacBook-Pro.local>
2019-11-28 15:47:35 -0500

    tweak

--- Private/InvokeTwitterGetApiCall.ps1
+++ Private/InvokeTwitterGetApiCall.ps1
@@ -19,6 +19,5 @@ function InvokeTwitterGetApiCall {
     $ApiParams.GetEnumerator() | Sort-Object -Property name | foreach { $HttpRequestUrl += "{0}={1}&" -f $_.Key, $_.Value }
     $HttpRequestUrl = $HttpRequestUrl.Trim('&')
     Write-Verbose "HTTP request URL is '$HttpRequestUrl'"
-    Invoke-RestMethod -URI $HttpRequestUrl -Method 'GET' -Headers @{ 'Authorization' = $AuthorizationString } -ContentType 'application/json'
-    
+    Invoke-RestMethod -URI $HttpRequestUrl -Method 'GET' -Headers @{ 'Authorization' = $AuthorizationString } -ContentType 'application/json'   
 }
