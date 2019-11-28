5dfc8e07f31af0955a1d73f2ed9c1c614c4b3294
Adam Bertram <adam@Adams-MacBook-Pro.local>
2019-11-28 13:53:39 -0500

    Added GetUserFollowerId

--- Private/GetUserFollowerId.ps1
+++ Private/GetUserFollowerId.ps1
@@ -1,4 +1,4 @@
-function Get-UserFollower {
+function GetUserFollowerId {
     [CmdletBinding()]
     param
     (
@@ -9,16 +9,16 @@ function Get-UserFollower {
 
     $ErrorActionPreference = 'Stop'
 
-    $HttpEndPoint = 'https://api.twitter.com/1.1/users/lookup.json'
+    $HttpEndPoint = 'https://api.twitter.com/1.1/followers/ids.json'
     $ApiParams = @{
         'screen_name' = $ScreenName
     }
 
     $AuthorizationString = Get-OAuthAuthorization -ApiParameters $ApiParams -HttpEndPoint $HttpEndPoint -HttpVerb GET
 		
-    $HttpRequestUrl = "$HttpEndPoint?"
+    $HttpRequestUrl = "$HttpEndPoint`?"
     $ApiParams.GetEnumerator() | Sort-Object -Property name | foreach { $HttpRequestUrl += "{0}={1}&" -f $_.Key, $_.Value }
     $HttpRequestUrl = $HttpRequestUrl.Trim('&')
     Write-Verbose "HTTP request URL is '$HttpRequestUrl'"
-    Invoke-RestMethod -URI $HttpRequestUrl -Method Get -Headers @{ 'Authorization' = $AuthorizationString } -ContentType "application/json"   
+    Invoke-RestMethod -URI $HttpRequestUrl -Method Get -Headers @{ 'Authorization' = $AuthorizationString } -ContentType "application/json"
 }
